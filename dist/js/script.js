bde7a46ead4334f3b8064b240f20d26c87e9f04d
Excelsio <vapeexcelsio@gmail.com>
2024-08-08 18:34:10 +0300

    adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener("click", () => {
 closeElem.addEventListener("click", () => {
     menu.classList.remove("active");
 });
+
+const counters = document.querySelectorAll(".progress__percentage"),
+    lines = document.querySelectorAll(".progress__bar span");
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
